1d22d60c1bebca6c8ec57fa3ccc5e1c6e0411d5f
rasc0 <oscartrodgers1@gmail.com>
2020-05-09 09:49:51 +1000

    Update hanoi.py
    Added comments

--- hanoi.py
+++ hanoi.py
@@ -5,21 +5,21 @@ A = []
 B = []
 C = []
 
-def showP():
+def showP(): #show pegs
     print("A: ",*A, sep = " ")
     print("B: ",*B, sep = " ")
     print("C: ",*C, sep = " ") 
     print("----------------------------")
     time.sleep(0.5)
     
-def move(f,t):
+def move(f,t): #move from f to t
     global moveCount
     t.append(f[-1])
     f.pop(-1)
     moveCount += 1
     showP()
 
-def move1():
+def move1(): # move disk 1 across the "board"
     if 1 in A:
         move(A,B)
         move(B,C)
@@ -27,7 +27,7 @@ def move1():
         move(C,B)
         move(B,A)    
 
-def hanoi(n): 
+def hanoi(n): # main function
     global A
     for x in range(n,0,-1):
         A.append(x)
@@ -40,7 +40,7 @@ def hanoi(n):
         if A: 
             a = A[-1]
         else:
-            a = n+1
+            a = n+1 # if the array for the peg is empty set its "top value" to be 1 greater than n, thus any disk can move to it.
             
         if B: 
             b = B[-1]
@@ -50,7 +50,7 @@ def hanoi(n):
             c = C[-1]
         else: c = n+1
         
-        if a < b and a != 1:
+        if a < b and a != 1: # not 1 as disk 1 is only moved in method move1()
             move(A,B)
         elif b < c and b != 1:
             move(B,C)
